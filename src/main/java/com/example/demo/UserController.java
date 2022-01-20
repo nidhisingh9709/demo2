cab05e9d8c66b882221339c83a0960816958ab59
nidhisingh2224 <tcslearning2422@gmail.com>
2022-01-20 10:29:36 +0530

    delete method

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,5 +20,9 @@ public class UserController {
 		System.out.println(user.getName());
 		System.out.println("Hello");
 		//new things to be added
+	}
+	@DeleteMapping("/user")
+	void deleteUser() {
+		System.out.println("deleted");
 	}
 }
